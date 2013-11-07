2f0032b8dbec6fd385e0cc8512eef3aee8f8784a
Soledad Penades <sole@mozilla.com>
2013-11-07 21:03:42 +0000

    trying to minimise worker code just in case it's the reason why it is not loading in Gaia 1.0 (?)

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,7 @@
 cat NeuQuant.js quantizer.js > tmpworker.js
+uglifyjs tmpworker.js --output tmpworker.min.js
 echo 'module.exports = function() { ' > header.txt
 echo '}' > footer.txt
-cat header.txt tmpworker.js footer.txt > worker.js
+cat header.txt tmpworker.min.js footer.txt > worker.js
 browserify exporter.js > ../dist/Animated_GIF.js
 rm header.txt footer.txt worker.js tmpworker.js
